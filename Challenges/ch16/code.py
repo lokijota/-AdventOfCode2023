ffdb99f782214260fdba7fc19ad07302b6f23c50
Jota <joao.pedro.martins@gmail.com>
2023-12-17 16:36:46 +0100

    Scaffolding for challenge 16. Now out for a capuccino

---
+++ Challenges/ch16/code.py
@@ -0,0 +1,37 @@
+import numpy as np
+from collections import Counter
+import re
+import copy
+from tqdm import tqdm
+import time
+
+# functions
+
+# main code
+
+# read all the lines
+with open('Challenges/ch15/input.txt') as f:
+    lines = f.read().splitlines()
+
+# parse data file content
+
+
+# global variables
+
+# functions
+
+# process data
+
+start_time = time.time()
+
+result = 0
+
+print("Result part 1: ", result)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# part 2
+
+result = 0
+
+print("Result part 2: ", result)
