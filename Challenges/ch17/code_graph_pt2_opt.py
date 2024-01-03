83c12b3efc1eab45234a3bda0c974ec407b9e939
Jota <joao.pedro.martins@gmail.com>
2024-01-03 02:36:31 +0000

    Ch17 part 2... in a second instead of 8 hours. Maggie fixed the final bug

--- Challenges/ch17/code_graph_pt2_opt.py
+++ Challenges/ch17/code_graph_pt2_opt.py
@@ -50,14 +50,13 @@ def printPath(map, path):
                 print(map[r][c], end="")
         print()
 
-
-def get_possible_next_nodes(current_min_node, minSteps, maxSteps):
+def get_possible_next_nodes(current_min_node, min_steps, maxSteps):
     "Returns all possible next nodes, for all possible movement directions considering the current node"
 
     if current_min_node[0] == 0 and current_min_node[1] == 0:
         if current_min_node[2] == "X": # start node
-            vert = [(sum(map[current_min_node[0]+1:current_min_node[0]+j, current_min_node[1]]),(current_min_node[0]+j ,current_min_node[1],"S")) for j in range(minSteps, maxSteps+1)]
-            horz = [(sum(map[current_min_node[0], current_min_node[1]+1:current_min_node[1]+j]),(current_min_node[0],current_min_node[1]+j,"E")) for j in range(minSteps, maxSteps+1)]
+            vert = [(sum(map[current_min_node[0]+1:current_min_node[0]+j+1, current_min_node[1]]),(current_min_node[0]+j ,current_min_node[1],"S")) for j in range(min_steps, maxSteps+1)]
+            horz = [(sum(map[current_min_node[0], current_min_node[1]+1:current_min_node[1]+j+1]),(current_min_node[0],current_min_node[1]+j,"E")) for j in range(min_steps, maxSteps+1)]
             return vert + horz
         else:
             return [] #repeat visits to 0,0 make no sense
@@ -67,14 +66,14 @@ def get_possible_next_nodes(current_min_node, minSteps, maxSteps):
         vertS = []
 
         # North
-        if current_min_node[0] >= minSteps:
+        if current_min_node[0] >= min_steps:
             real_max = min(maxSteps, current_min_node[0])
-            vertN = [(sum(map[current_min_node[0]-j:current_min_node[0]-1, current_min_node[1]]),(current_min_node[0]-j ,current_min_node[1],"N")) for j in range(minSteps, real_max+1)]
+            vertN = [(sum(map[current_min_node[0]-j:current_min_node[0], current_min_node[1]]),(current_min_node[0]-j ,current_min_node[1],"N")) for j in range(min_steps, real_max+1)]
 
         # South
-        if current_min_node[0] <= nrows - minSteps: 
+        if current_min_node[0] <= nrows - min_steps: 
             real_max = min(maxSteps, nrows-1-current_min_node[0])
-            vertS = [(sum(map[current_min_node[0]+1:current_min_node[0]+j, current_min_node[1]]),(current_min_node[0]+j ,current_min_node[1],"S")) for j in range(minSteps, real_max+1)]
+            vertS = [(sum(map[current_min_node[0]+1:current_min_node[0]+j+1, current_min_node[1]]),(current_min_node[0]+j ,current_min_node[1],"S")) for j in range(min_steps, real_max+1)]
 
         return vertN + vertS
 
@@ -83,19 +82,18 @@ def get_possible_next_nodes(current_min_node, minSteps, maxSteps):
         horzW = []
 
         # East
-        if current_min_node[1] <= ncols - minSteps: 
+        if current_min_node[1] <= ncols - min_steps: 
             real_max = min(maxSteps, ncols-1-current_min_node[1])
-            horzE = [(sum(map[current_min_node[0], current_min_node[1]+1:current_min_node[1]+j]),(current_min_node[0],current_min_node[1]+j,"E")) for j in range(minSteps, real_max+1)]
+            horzE = [(sum(map[current_min_node[0], current_min_node[1]+1:current_min_node[1]+j+1]),(current_min_node[0],current_min_node[1]+j,"E")) for j in range(min_steps, real_max+1)]
 
         # West
-        if current_min_node[1] >= minSteps:
+        if current_min_node[1] >= min_steps:
             real_max = min(maxSteps, current_min_node[1])
-            horzW = [(sum(map[current_min_node[0], current_min_node[1]-j:current_min_node[1]-1]),(current_min_node[0],current_min_node[1]-j,"W")) for j in range(minSteps, real_max+1)]
+            horzW = [(sum(map[current_min_node[0], current_min_node[1]-j:current_min_node[1]]),(current_min_node[0],current_min_node[1]-j,"W")) for j in range(min_steps, real_max+1)]
 
         return horzE + horzW
 
     assert True, "I should not be here"
-
 
 def dijkstra_algorithm_with_priority_queue(graph, start_node):
     shortest_path = {}
@@ -110,7 +108,6 @@ def dijkstra_algorithm_with_priority_queue(graph, start_node):
 
         # get node with the lowest score
         _, current_min_node = heapq.heappop(nodes_to_visit)
-        print(current_min_node)
 
         # let's get the neighbours
         neighbors = get_possible_next_nodes(current_min_node, 4, 10)
@@ -130,62 +127,6 @@ def dijkstra_algorithm_with_priority_queue(graph, start_node):
 
     return previous_nodes, shortest_path 
 
-
-
-def dijkstra_algorithm(graph, start_node):
-    "Applies Dijkstra's shortest path algorithm"
-    unvisited_nodes = list(graph.get_nodes())
- 
-    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph   
-    shortest_path = {}
- 
-    # We'll use this dict to save the shortest known path to a node found so far
-    previous_nodes = {}
- 
-    # We'll use max_value to initialize the "infinity" value of the unvisited nodes   
-    max_value = sys.maxsize
-    for node in unvisited_nodes:
-        shortest_path[node] = max_value
-    # However, we initialize the starting node's value with 0   
-    shortest_path[start_node] = 0
-    
-    # The algorithm executes until we visit all nodes
-    # JOTA edit
-    pbar = tqdm(total=len(unvisited_nodes)+1)
-    # JOTA end edit
-
-    while unvisited_nodes:
-        # The code block below finds the node with the lowest score
-        current_min_node = None
-        for node in unvisited_nodes: # Iterate over the nodes
-            if current_min_node == None:
-                current_min_node = node
-            elif shortest_path[node] < shortest_path[current_min_node]:
-                current_min_node = node
-        
-        # The code block below retrieves the current node's neighbors and updates their distances
-        neighbors = graph.get_outgoing_edges(current_min_node)
-        for neighbor in neighbors:
-            # JOTA edit - prevent revisiting nodes
-            if current_min_node in previous_nodes:
-                prev_visited_node = previous_nodes[current_min_node]
-                if prev_visited_node[0] == neighbor[0] and prev_visited_node[1] == neighbor[1]:
-                    continue
-            # JOTA end edit
-            
-            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
-            if tentative_value < shortest_path[neighbor]:
-                shortest_path[neighbor] = tentative_value
-                # We also update the best path to the current node
-                previous_nodes[neighbor] = current_min_node
-
-        # After visiting its neighbors, we mark the node as "visited"
-        unvisited_nodes.remove(current_min_node)
-        pbar.update(1) # JOTA added
-    
-    pbar.close() # JOTA added
-
-    return previous_nodes, shortest_path
 
 def print_result(previous_nodes, shortest_path, start_node, target_node):
     "Auxiliary function to print the path and map in a convenient way and returns the sum of the cost"
@@ -221,19 +162,18 @@ for p in shortest_path:
         if shortest_path[p] < min_dist:
             min_dist = shortest_path[p]
             min_node = p
-assert min_dist != 99999999, "Path not found!"
-
-print("Node with lowest cost:", min_node, "Cost:", min_dist)
 
 result = print_result(previous_nodes, shortest_path, start_node=(0,0, "X"), target_node=min_node)
 
-print(len(previous_nodes), len(shortest_path))
 print("Result part 2: ", result) 
 print("--- %s seconds ---" % (time.time() - start_time))
 
 
+# OLD IMPLEMENTATION...
 # Result part 2:  1416
 # --- 27427.103300094604 seconds ---
 # ^right answer after 7.6 hours!... not good. I had a bug and was only doing 9 in a row and not 10.
 
-# 1152 -- muito baixo, n está a calcular bem
+# NEW IMPLEMENTATION: !!!
+# Result part 2:  1416
+# --- 0.9768238067626953 seconds ---
