c5a906633b25bd60e3b0a24232863543e0584352
Jota <joao.pedro.martins@gmail.com>
2024-08-27 23:52:11 +0200

    Ch21 base code file

---
+++ Challenges/ch21/code.py
@@ -0,0 +1,44 @@
+import sys
+import numpy as np
+from collections import Counter
+import re
+import copy
+from tqdm import tqdm
+import time
+from collections import deque
+import random
+
+# functions
+
+# main code
+
+# read all the lines
+with open('Challenges/ch21/sample.txt') as f:
+    lines = f.read().splitlines()
+
+# parse data file content
+
+for line in lines:
+    print(line)
+
+# global variables
+result = 0
+
+# process data
+
+
+# part 1
+start_time = time.time()
+
+print("Result part 1: ", result) 
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# part 2 
+
+# input("*********** Press Enter to continue... **********")
+start_time = time.time()
+
+
+
+print("Result part 2: ", result) # 
+print("--- %s seconds ---" % (time.time() - start_time))
