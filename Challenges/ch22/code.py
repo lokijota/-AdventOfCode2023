6a56245b9bf87d92ef4d1fb4219d245898e45f78
Jota <joao.pedro.martins@gmail.com>
2024-10-01 23:00:25 +0200

    ch22 base setup

---
+++ Challenges/ch22/code.py
@@ -0,0 +1,47 @@
+import time
+from collections import deque
+import sys
+import numpy as np
+from collections import Counter
+import re
+import copy
+from tqdm import tqdm
+import random
+
+
+# functions
+
+
+# main code
+
+# read all the lines
+with open('Challenges/ch22/input.txt') as f:
+    lines = f.read().splitlines()
+
+map = lines
+
+# global variables
+
+# process data
+
+# part 1
+start_time = time.time()
+result = 0
+
+
+print(f"Result part 1: {result}")
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# part 2
+
+
+# for r in range(0,8):
+#     print(f"Area of {r} is", calculate_area_losange(r))
+
+
+
+# input("*********** Press Enter to continue... **********")
+# start_time = time.time()
+# print("Result part 2: ", result) #
+# print("--- %s seconds ---" % (time.time() - start_time))
+
