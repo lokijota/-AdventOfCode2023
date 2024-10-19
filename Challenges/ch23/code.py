9f69922f7efd09137aa73c084f063f10e98ee17b
Jota <joao.pedro.martins@gmail.com>
2024-10-20 01:02:57 +0200

    Scaffolding ch23

---
+++ Challenges/ch23/code.py
@@ -0,0 +1,41 @@
+import time
+from collections import deque
+import sys
+import numpy as np
+from collections import Counter
+import re
+import copy
+from tqdm import tqdm
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions
+
+# main code
+
+# read all the lines
+with open('Challenges/ch23/sample.txt') as f:
+    lines = f.read().splitlines()
+
+
+# global variables
+
+# process data
+
+# part 1
+start_time = time.time()
+result = 0
+
+
+print(f"Result part 1: {result}")
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# part 2
+
+# input("*********** Press Enter to continue... **********")
+# start_time = time.time()
+# print("Result part 2: ", result) #
+# print("--- %s seconds ---" % (time.time() - start_time))
+
